6600d8b09081e66e4561094f5c7059bdefceee7f
eejai42 <start@anabstractlevel.com>
2018-06-14 22:43:37 -0500

    Tweaked main.go to add line breaks.

--- main.go
+++ main.go
@@ -9,18 +9,20 @@ import (
 func main() {
 
 	world := new(demo.World)
-	world.Init("amqp://guest:guest@localhost/demo")
+	world.Init("amqp://guest:guest@localhost/DEMO")
 	world.AddProgrammerHelloHandler(func(actor *demo.ActorBase, payload *demo.Payload) *demo.Payload {
+		fmt.Println("" + payload.Content)
 		fmt.Println("Got hello from programmer" + payload.Content)
 		return payload
 	})
 
 	programmer := new(demo.Programmer)
 
-	programmer.Init("amqp://guest:guest@localhost/demo")
+	programmer.Init("amqp://guest:guest@localhost/DEMO")
 	payload := programmer.CreatePayload()
 	payload.Content = "This is the payload"
 	programmer.Hello(payload, func(actor *demo.ActorBase, reply *demo.Payload) *demo.Payload {
+		fmt.Println("")
 		fmt.Println("Got reply from the world")
 		return reply
 	})
